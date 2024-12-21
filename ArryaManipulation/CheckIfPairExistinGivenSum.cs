2ce62dd1566d58f9ab775ad67811d9888f181776
koya171 <narendrakoya09@gmail.com>
2024-12-21 15:25:33 +0530

    changes

--- ArryaManipulation/CheckIfPairExistinGivenSum.cs
+++ ArryaManipulation/CheckIfPairExistinGivenSum.cs
@@ -1,113 +1,113 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Net;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Net;
+//using System.Text;
 
 
-namespace ArryaManipulation
-{
-    class CheckIfPairExistinGivenSum
-    {
-        public static void Main()
-        {
-            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            int tragetValue = 10;
+//namespace ArryaManipulation
+//{
+//    class CheckIfPairExistinGivenSum
+//    {
+//        public static void Main()
+//        {
+//            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//            int tragetValue = 10;
 
-            if (HasPairWithSum2(arr, tragetValue))
-            {
-                Console.WriteLine("There is apair with the given Sum.");
-            }
-            else
-            {
-                Console.WriteLine("No pair found with the given sum.");
-            }
-        }
+//            if (HasPairWithSum2(arr, tragetValue))
+//            {
+//                Console.WriteLine("There is apair with the given Sum.");
+//            }
+//            else
+//            {
+//                Console.WriteLine("No pair found with the given sum.");
+//            }
+//        }
 
-        private static bool HasPairWithSum(int[] arr, int tragetValue)
-        {
+//        private static bool HasPairWithSum(int[] arr, int tragetValue)
+//        {
 
-            if (arr.Length == 0)
-            {
-                return false;
-            }
-            for (int i = 0; i < arr.Length - 1; i++)
-            {
+//            if (arr.Length == 0)
+//            {
+//                return false;
+//            }
+//            for (int i = 0; i < arr.Length - 1; i++)
+//            {
 
-                for (int j = i + 1; j < arr.Length; j++)
-                {
-                    if (arr[i] + arr[j] == tragetValue)
-                    {
-                        Console.WriteLine($"pair found:{arr[i]},{arr[j]}");
-                        return true;
-                    }
-                }
+//                for (int j = i + 1; j < arr.Length; j++)
+//                {
+//                    if (arr[i] + arr[j] == tragetValue)
+//                    {
+//                        Console.WriteLine($"pair found:{arr[i]},{arr[j]}");
+//                        return true;
+//                    }
+//                }
 
-            }
-            return false;
+//            }
+//            return false;
 
-        }
-        private static bool HasPairWithSum1(int[] arr, int tragetValue)
-        {
-            if (arr.Length == 0)
-            {
-                return false;
-            }
+//        }
+//        private static bool HasPairWithSum1(int[] arr, int tragetValue)
+//        {
+//            if (arr.Length == 0)
+//            {
+//                return false;
+//            }
 
-            int left = 0;
-            int right = arr.Length - 1;
+//            int left = 0;
+//            int right = arr.Length - 1;
 
-            while (left < right)
-            {
-                int currentSum = arr[left] + arr[right];
+//            while (left < right)
+//            {
+//                int currentSum = arr[left] + arr[right];
 
-                if (currentSum == tragetValue)
-                {
-                    Console.WriteLine($"pair found (:{arr[left]},{arr[right]})");
-                    return true;
-                }
-                else if (currentSum < currentSum)
-                {
-                    left++;
-                }
-                else
-                {
-                    right--;
-                }
-            }
+//                if (currentSum == tragetValue)
+//                {
+//                    Console.WriteLine($"pair found (:{arr[left]},{arr[right]})");
+//                    return true;
+//                }
+//                else if (currentSum < tragetValue)
+//                {
+//                    left++;
+//                }
+//                else
+//                {
+//                    right--;
+//                }
+//            }
 
-            return false;
+//            return false;
 
-        }
+//        }
 
-        private static bool HasPairWithSum2(int[] arr, int tragetValue)
-        {
-            if (arr.Length == 0)
-            {
-                return false;
-            }
-            Dictionary<int, int> hasmap = new Dictionary<int, int>();
-            for (int i = 0; i < arr.Length - 1; i++)
-            {
-                int complement = tragetValue - arr[i];
-                if (hasmap.ContainsKey(complement))
-                {
-                    int complementIndex = hasmap[complement];
-                    Console.WriteLine($"pair find :{arr[complementIndex]},{arr[i]}");
-                    return true;
-                }
-                if (!hasmap.ContainsKey(arr[i]))
-                {
-                    hasmap.Add(arr[i], i);
-                }
-            }
-            return false;
-        }
-    }
-}
+//        private static bool HasPairWithSum2(int[] arr, int tragetValue)
+//        {
+//            if (arr.Length == 0)
+//            {
+//                return false;
+//            }
+//            Dictionary<int, int> hasmap = new Dictionary<int, int>();
+//            for (int i = 0; i < arr.Length - 1; i++)
+//            {
+//                int complement = tragetValue - arr[i];
+//                if (hasmap.ContainsKey(complement))
+//                {
+//                    int complementIndex = hasmap[complement];
+//                    Console.WriteLine($"pair find :{arr[complementIndex]},{arr[i]}");
+//                    return true;
+//                }
+//                if (!hasmap.ContainsKey(arr[i]))
+//                {
+//                    hasmap.Add(arr[i], i);
+//                }
+//            }
+//            return false;
+//        }
+//    }
+//}
 
 
 
--- ArryaManipulation/Program.cs
+++ ArryaManipulation/Program.cs
@@ -10,10 +10,42 @@ using System.Text;
 
 namespace ArryaManipulation
 {
-    class Findtriplets
+    class TwoSumArrays
     {
         public static void Main()
         {
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 100;
+            int[] result = TwoSumArray(nums, target);
+            Console.WriteLine($"Indices: [{result[0]}, {result[1]}]");
+        }
+ 
+          private static int[] TwoSumArray(int[] arr, int target)
+        {
+            // Dictionary to store numbers and their indices
+            Dictionary<int, int> hasmap = new Dictionary<int, int>();
+
+            // Iterate through the array
+            for (int i = 0; i < arr.Length; i++)
+            {
+                // Calculate the complement
+                int complement = target - arr[i];
+                // Check if the complement exists in the dictionary
+                if (hasmap.ContainsKey(complement))
+                {
+                    // Return the indices of the two numbers
+                    return new int[] { hasmap[complement], i };
+                }
+                if (!hasmap.ContainsKey(complement))
+                {
+                    // Add the current number and its index to the dictionary
+                    hasmap.Add(arr[i], i);
+                }
+               
+            }
+
+            // No solution found (though guaranteed by the problem description)
+            throw new ArgumentException("No solution found");
         }
 
       
